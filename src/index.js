182645c25745e36b804256a7a182c55e38da5b84
zhoupeng <zhoupeng1995@live.com>
2019-07-05 18:25:50 +0800

    保存

--- src/index.js
+++ src/index.js
@@ -1,31 +1,54 @@
 import Vue from './vue/platforms/web/entry-runtime-with-compiler'
 // console.log(Vue)
 debugger
-Vue.component('deep-2', {
-    template: '<div>deep2</div>',
-    created() {
-        console.log('child-component-1-1 created')
-    },
+// Vue.component('deep-2', {
+//     template: '<div>deep2</div>',
+//     created() {
+//         console.log('child-component-1-1 created')
+//     },
 
-    mounted() {
-        console.log('child-component-1-1 mounted')
-    }
-})
-Vue.component('my-component', { 
-    template: '<div><deep-2></deep-2>deep1</div>',
-    created() {
-        console.log('child-component-1 created')
-    },
+//     mounted() {
+//         console.log('child-component-1-1 mounted')
+//     }
+// })
+// Vue.component('my-component', { 
+//     template: '<div><deep-2></deep-2>deep1</div>',
+//     created() {
+//         console.log('child-component-1 created')
+//     },
 
-    mounted() {
-        console.log('child-component-1 mounted')
-    }
-})
-
-new Vue({
+//     mounted() {
+//         console.log('child-component-1 mounted')
+//     }
+// })
+let count = 0
+window.vueInstance = new Vue({
     el: '#app',
     data: {
-        test: 123
+        test: 123,
+        a: 1,
+        b: 2
+    },
+    directives: {
+        align: {
+            bind: function (el, binding, vNode, oldVNode) {
+                console.log(1)
+                // debugger
+                // vNode.key = ++count
+                const span = document.createElement('span')
+                span.innerText = el.innerText
+                el.innerHTML = ''
+                el.appendChild(span)
+            },
+
+            update: function(el, binding, newNode, oldVNode) {
+                console.log(2, el, newNode, oldVNode)
+                const span = document.createElement('span')
+                span.innerText = el.innerText
+                el.innerHTML = ''
+                el.appendChild(span)
+            }
+        }
     },
     created() {
         console.log('father created')
